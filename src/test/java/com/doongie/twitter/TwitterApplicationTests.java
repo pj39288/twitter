74cadcd225a381f3f72a76dcde6dde1690bc23ef
사용자 <pj39288@hanmail.net>
2023-10-03 14:04:58 +0900

    twitter 최초 업로드

---
+++ src/test/java/com/doongie/twitter/TwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doongie.twitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
